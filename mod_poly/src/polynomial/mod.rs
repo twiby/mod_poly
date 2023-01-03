e1ea647f072c448367a95a3228065a27c56d821d
twiby <t.balenbois@gmail.com>
2023-01-03 14:58:43 +0100

    Add type to represent polynomials in a modular arithmetic

--- mod_poly/src/polynomial/mod.rs
+++ mod_poly/src/polynomial/mod.rs
@@ -7,15 +7,6 @@ mod test;
 
 use crate::complex::Number;
 
-/// Type defining a general polynomial: 
-/// We store all coefficients in a Vec, its index in the Vec reprenting its degree.
-///  If the maximum degree can be known at compile time, this could be on the stack, 
-/// and be made much more efficiently.
-#[derive(Clone)]
-pub struct Polynomial<T: Number> {
-	coefs: Vec<T>	
-}
-
 /// Efficient integer power function, mercilessly stolen from num crate (unused for now)
 #[inline]
 pub fn pow<T: Number>(mut base: T, mut exp: usize) -> T {
@@ -36,6 +27,15 @@ pub fn pow<T: Number>(mut base: T, mut exp: usize) -> T {
 		}
 	}
 	acc
+}
+
+/// Type defining a general polynomial: 
+/// We store all coefficients in a Vec, its index in the Vec reprenting its degree.
+/// TODO: If the maximum degree can be known at compile time, this could be on the stack, 
+/// and be made much more efficiently.
+#[derive(Clone)]
+pub struct Polynomial<T: Number> {
+	coefs: Vec<T>	
 }
 
 impl<T: Number> Polynomial<T> {
@@ -63,3 +63,40 @@ impl<T: Number> Polynomial<T> {
 		ret
 	}
 }
+
+/// Type representing a polynomial mod(x^modulus - 1)
+#[derive(Clone)]
+pub struct ModularArithmeticPolynomial<T: Number> {
+	modulus: usize,
+	polynomial: Polynomial<T>
+}
+
+impl<T: Number> ModularArithmeticPolynomial<T> {
+	/// Polynomial doesn't need to already respect the modular arithmetic
+	pub fn new(poly: &Polynomial<T>, modulus: usize) -> Self {
+		Self{polynomial: Self::sanitize(poly, modulus), modulus: modulus}
+	}
+
+	/// Calls the underlying polynomial call function
+	pub fn apply(&self, x: T) -> T {
+		self.polynomial.apply(x)
+	}
+
+	/// Computes a lowest degree polynomial congruent to the input one in the modular arithmetic
+	fn sanitize(poly: &Polynomial<T>, modulus: usize) -> Polynomial<T> {
+		let size = poly.coefs.len();
+		if size < modulus { return poly.clone(); }
+
+		let mut ret = Polynomial::<T>::new(&poly.coefs[0..modulus]);
+
+		let mut reduced_i = 0;
+		for i in modulus..size {
+			ret.coefs[reduced_i] += poly.coefs[i]; 
+			reduced_i += 1;
+			reduced_i -= ((reduced_i == modulus) as usize) * modulus;
+		}
+
+		ret
+	}
+}
+
--- mod_poly/src/polynomial/test.rs
+++ mod_poly/src/polynomial/test.rs
@@ -1,7 +1,7 @@
 use crate::complex::{Complex, I_F32};
 
 use crate::polynomial;
-use crate::polynomial::Polynomial;
+use crate::polynomial::{Polynomial, ModularArithmeticPolynomial};
 
 #[test]
 fn pow_basic() {
@@ -68,4 +68,32 @@ fn monomial() {
 	let poly_1 = Polynomial::new_monomial(I_F32, 2);
 
 	assert_eq!(poly_1.apply(Complex::<f32>::from(2.0)), I_F32 * Complex::<f32>::from(4.0));
+}
+
+#[test]
+fn mod_polynomial() {
+	let mono_1 = Polynomial::new_monomial(1.0, 1);
+	let mono_2 = Polynomial::new_monomial(1.0, 2);
+	let mono_3 = Polynomial::new_monomial(1.0, 3);
+	let mono_4 = Polynomial::new_monomial(1.0, 4);
+	let mono_5 = Polynomial::new_monomial(1.0, 5);
+	let mono_6 = Polynomial::new_monomial(1.0, 6);
+
+	let mut mod_poly = ModularArithmeticPolynomial::new(&mono_1, 3);
+	assert_eq!(mod_poly.apply(2.0), 2.0);
+
+	mod_poly = ModularArithmeticPolynomial::new(&mono_2, 3);
+	assert_eq!(mod_poly.apply(2.0), 4.0);
+
+	mod_poly = ModularArithmeticPolynomial::new(&mono_3, 3);
+	assert_eq!(mod_poly.apply(2.0), 1.0);
+
+	mod_poly = ModularArithmeticPolynomial::new(&mono_4, 3);
+	assert_eq!(mod_poly.apply(2.0), 2.0);
+
+	mod_poly = ModularArithmeticPolynomial::new(&mono_5, 3);
+	assert_eq!(mod_poly.apply(2.0), 4.0);
+
+	mod_poly = ModularArithmeticPolynomial::new(&mono_6, 3);
+	assert_eq!(mod_poly.apply(2.0), 1.0);
 }
